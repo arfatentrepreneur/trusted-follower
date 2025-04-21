3e95d3e3fc08d8e0244f262baa6ab9e7a15fe8c7
arfatentrepreneur <Arfatkingshaikh65@gmail.com>
2025-04-22 00:20:23 +0530

    Update boostpanel.js

--- server/services/boostpanel.js
+++ server/services/boostpanel.js
@@ -1,25 +1,9 @@
-const axios = require('axios');
-
-// Mock BoostPanel API
-const boostPanel = {
-  url: '/boostpanel/mock-api', // Simulated endpoint
-  key: 'mock_key',
-  serviceId: 'instagram_likes_30',
-};
-
 const placeOrder = async (postUrl, quantity) => {
-  try {
-    const response = await axios.post(boostPanel.url, {
-      key: boostPanel.key,
-      action: 'add',
-      service: boostPanel.serviceId,
-      link: postUrl,
-      quantity,
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error(error.response?.data?.error || 'BoostPanel error');
-  }
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ success: true, message: `${quantity} likes ordered` });
+    }, 1000);
+  });
 };
 
 module.exports = { placeOrder };
