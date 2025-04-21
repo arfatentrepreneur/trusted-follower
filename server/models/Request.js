a445c5de3f33a07003b3578dceecf7b04e642c27
arfatentrepreneur <Arfatkingshaikh65@gmail.com>
2025-04-22 00:10:27 +0530

    Update Request.js

--- server/models/Request.js
+++ server/models/Request.js
@@ -7,7 +7,11 @@ const findOne = (query) => {
 
 const findOneAndUpdate = (query, update, options) => {
   const index = requests.findIndex((req) => req.userIp === query.userIp);
-  const newRequest = { userIp: query.userIp, lastRequest: update.lastRequest };
+  const newRequest = {
+    userIp: query.userIp,
+    lastRequest: update.lastRequest,
+    requestCount: update.requestCount || (index === -1 ? 1 : (requests[index].requestCount || 0) + 1)
+  };
   if (index === -1) {
     requests.push(newRequest);
   } else {
