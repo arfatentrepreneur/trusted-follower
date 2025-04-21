f2b63e191970b805c946063919cee20970fc69f2
arfatentrepreneur <Arfatkingshaikh65@gmail.com>
2025-04-21 15:47:15 +0530

    Create App.js

---
+++ client/src/styles/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FreeLikes from './components/FreeLikes';
+
+function App() {
+  return (
+    <div>
+      <FreeLikes />
+    </div>
+  );
+}
+
+export default App;
