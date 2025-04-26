0ce11ceff2ccabf00b05e456171784e98ab45528
arfatentrepreneur <Arfatkingshaikh65@gmail.com>
2025-04-26 23:31:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Ya jo port chahiye
+const port = 3000;
 
-// Middleware
 app.use(express.json());
 
-// Routes
 const instagramRoutes = require('../routes/instagram-routes');
 app.use('/api', instagramRoutes);
 
-// Server Start
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
