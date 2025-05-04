3641144bc17c2cc2d3d5bf2731928b92426f62a4
arfatentrepreneur <Arfatkingshaikh65@gmail.com>
2025-05-04 17:17:50 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 try {
-  const instagramRoutes = require('./server/routes/instagram-routes');
+  const instagramRoutes = require('./routes/instagram-routes');
   console.log('File loaded successfully:', instagramRoutes);
 } catch (error) {
   console.error('Error loading file:', error);
