1c92dd0d83bfe4a78042f283af5c8a67bc658a63
arfatentrepreneur <Arfatkingshaikh65@gmail.com>
2025-05-08 21:56:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const instagramRoutes = require('./routes/instagram-routes');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-const instagramRoutes = require('./routes/instagram-routes');
 app.use('/api', instagramRoutes);
 
 app.listen(port, () => {
